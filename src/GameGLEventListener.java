994df804e75f63c84d6375f7ee8c9b37207a88d9
fatma <fatmatnt4@gmail.com>
2025-12-05 15:40:42 +0200

    add ball update

--- src/GameGLEventListener.java
+++ src/GameGLEventListener.java
@@ -3,6 +3,14 @@ import java.awt.event.KeyEvent;
 
 public class GameGLEventListener extends GameListener{
 
+    private double xBall=0;
+    private double yBall=0;
+    private double dxBall=0.02;
+    private double dyBall=0.01;
+    private double xmax=350;
+    private double xmin=-350;
+    private double ymax=350;
+    private double ymin=-350;
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
 
@@ -36,6 +44,17 @@ public class GameGLEventListener extends GameListener{
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+    public void updateball(){
+        xBall += dxBall;
+        yBall += dyBall;
+        //تغيير الاتجاه لو الكرة خبطت فى الحواف
+        if(xBall >= xmax ||xBall <= xmin){
+            dxBall = -dxBall;
+        }
+        if(yBall >= ymax ||yBall <= ymin){
+            dyBall = -dyBall;
+        }
     }
 }
 
